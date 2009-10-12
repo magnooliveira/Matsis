71309be618d512c25169826dce570a8485eb7280
magnooliveira <magnolimaoliveira@gmail.com>
2009-10-12 16:20:38 -0300

    alterei os nomes das variáveis

---
+++ Model/Sistema_De_Duas_Incognitas.py
@@ -0,0 +1,42 @@
+#Autor  : Magno Lima Oliveira
+#E-mail : magnolimaoliveira@gmail.com 
+
+'''
+>>> equacao = SistemaDuasIncognitas(4,-3,-3,2) 
+>>> equacao.acharDuasRetasCoincidentes()
+[8, -6, -6]
+>>> equacao.acharDuasRetasParalelas()
+[8, -6, -7]
+>>> equacao.acharDuasRetasConcorrentes()
+[9, -6, -6]
+
+'''
+
+import random
+from random import randrange
+
+class SistemaDuasIncognitas(object):
+
+	def __init__(self,coeficiente_a,coeficiente_b,coeficiente_c,k=random.randrange(2,6)):
+		self.equacao_Do_Usuario=[coeficiente_a,coeficiente_b,coeficiente_c]
+		self.k=k
+
+	def acharDuasRetasCoincidentes(self):
+		equacao_Do_Sistema=map(lambda x:x*self.k,self.equacao_Do_Usuario)
+		return equacao_Do_Sistema
+		
+	def acharDuasRetasParalelas(self):
+		equacao_Do_Sistema=map(lambda x:x*self.k,self.equacao_Do_Usuario)
+		equacao_Do_Sistema[len(self.equacao_Do_Usuario)-1]-=1
+		return equacao_Do_Sistema
+		
+	def acharDuasRetasConcorrentes(self): 
+		equacao_Do_Sistema=map(lambda x:x*self.k,self.equacao_Do_Usuario)
+		equacao_Do_Sistema[0]+=1
+		return equacao_Do_Sistema
+
+
+	
+if __name__=='__main__':
+	import doctest,Sistema_De_Duas_Incognitas
+	doctest.testmod()
