f62a30431d0f13ccdfe7434aeb12775fe4cdfe10
Magno <magnolimaoliveira@gmail.com>
2010-11-19 03:59:18 -0200

    Um script simples que serve para o MatSis e Dependencias

---
+++ paradigma.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Tem que estar como root
+# Exemplo:
+# sudo sh paradigma.sh
+#
+# Script em teste
+
+apt-get install python-matplotlib
+wget --no-check-certificate http://github.com/magnooliveira/matsis/tarball/master
+
