237382b6979981e16673409d3a5a887bb8f2e2e4
Magno <magnolimaoliveira@gmail.com>
2011-06-27 00:37:52 -0300

    Final

---
+++ MatSis.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding:UTF-8
+#Autor: Magno Lima Oliveira
+
+from sys import *
+import os.path
+
+caminho =  os.path.expanduser ("~/Matsis/View/interface_principal")
+path.append(caminho)
+
+from Menu import JanelaPrincipal
+main= JanelaPrincipal()
+
